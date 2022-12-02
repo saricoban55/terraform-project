040a6ff524a0e2115dc1f30d087023ddaea43ad4
lucardcoder <gokalpkocer@gmail.com>
2022-12-02 02:50:51 +0000

    added

---
+++ main/asg.tf
@@ -0,0 +1,113 @@
+
+resource "aws_security_group" "allow_tls" {
+  name        = "terraform-testing-alb"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+
+#Create ASG
+
+module "asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.3"
+  # Autoscaling group
+  name                      = "Project-asg"
+  min_size                  = 1
+  max_size                  = 3
+  desired_capacity          = 3
+  wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+  vpc_zone_identifier       = [aws_subnet.public1.id, aws_subnet.public2.id, aws_subnet.public3.id ]
+
+
+
+  # Launch template
+  launch_template_name        = "Project-asg"
+  launch_template_description = "Launch template example"
+  update_default_version      = true
+  image_id                    = "ami-0b0dcb5067f052a63"
+  instance_type               = "t3.micro"
+  ebs_optimized               = false
+  enable_monitoring           = false
+  user_data                   = base64encode(data.template_file.user_data.rendered)
+  target_group_arns           = module.alb.target_group_arns
+  security_groups = [
+    aws_security_group.allow_tls.id
+  ]
+}
+
+#Create ALB
+
+module "alb" {
+  source                           = "terraform-aws-modules/alb/aws"
+  version                          = "~> 8.0"
+  name                             = "my-alb"
+  load_balancer_type               = "application"
+  enable_cross_zone_load_balancing = true
+  vpc_id                           = aws_vpc.main.id
+  subnets                          = [aws_subnet.public1.id, aws_subnet.public2.id, aws_subnet.public3.id ]
+
+
+  security_groups = [
+    aws_security_group.allow_tls.id
+  ]
+
+  target_groups = [
+    {
+      name_prefix      = "pref-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+}
+
+
+
+
+
+
+
---
+++ main/output.tf
@@ -0,0 +1,23 @@
+# output "vpc_id" {
+#   value = aws_vpc.main.id
+# }
+# output "public_subnet1" {
+#   value = aws_subnet.public1.id
+# }
+# output "public_subnet2" {
+#   value = aws_subnet.public2.id
+# }
+# output "public_subnet3" {
+#   value = aws_subnet.public3.id
+# }
+# output "private_subnet1" {
+#   value = aws_subnet.private1.id
+# }
+# output "private_subnet2" {
+#   value = aws_subnet.private2.id
+# }
+# output "private_subnet3" {
+#   value = aws_subnet.private3.id
+# }
+
+
